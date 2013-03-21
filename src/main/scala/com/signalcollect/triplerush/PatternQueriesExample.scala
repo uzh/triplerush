e175954850d483b9fc5bbe3eb4dc2fa6db821367
Philip Stutz <philip.stutz@gmail.com>
2013-03-21 20:20:21 +0100

    It works, even on weird examples.

--- src/main/scala/com/signalcollect/triplerush/PatternQueriesExample.scala
+++ src/main/scala/com/signalcollect/triplerush/PatternQueriesExample.scala
@@ -11,17 +11,19 @@ object PatternQueriesExample extends App {
   val query1Dsl = SAMPLE(10000) ? "x" WHERE (
     | - "x" - s"$ub#takesCourse" - "http://www.Department0.University0.edu/GraduateCourse0",
     | - "x" - s"$rdf#type" - s"$ub#GraduateStudent")
-  val query2Dsl = SELECT ? "x" ? "y" ? "z" WHERE (
+  val query2Dsl = SAMPLE(100000) ? "x" ? "y" ? "z" WHERE (
     | - "x" - s"$rdf#type" - s"$ub#GraduateStudent",
     | - "x" - s"$ub#memberOf" - "z",
     | - "z" - s"$rdf#type" - s"$ub#Department",
     | - "z" - s"$ub#subOrganizationOf" - "y",
     | - "x" - s"$ub#undergraduateDegreeFrom" - "y",
     | - "y" - s"$rdf#type" - s"$ub#University") 
-  val query3Dsl = SELECT ? "x" WHERE (
+  val query3Dsl = SAMPLE(100000) ? "x" WHERE (
     | - "x" - s"$ub#publicationAuthor" - "http://www.Department0.University0.edu/AssistantProfessor0",
     | - "x" - s"$rdf#type" - s"$ub#Publication")
-
+  val crazyQuery = SAMPLE(100) ? "x" ?"y" ?"z" ? "b" WHERE (
+    | - "x" - "y" - "z",
+    | - "x" - s"$rdf#type" - "b")
   val qe = new QueryEngine
   println("Loading triples ...")
 
@@ -33,7 +35,7 @@ object PatternQueriesExample extends App {
   }
 
   println("Executing query ...")
-  qe.executeQuery(query1Dsl) onSuccess {
+  qe.executeQuery(crazyQuery) onSuccess {
     case results =>
       println("Result bindings:")
       results foreach { result =>
