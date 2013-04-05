4f7bf3290ad30f634987f130243382f184f21c92
Philip Stutz <philip.stutz@gmail.com>
2013-04-05 10:25:11 +0200

    Handled a case where a query might fail to bind to a triple.
    Could happen for patterns such as [ ?X blah ?X ].
    Renamed the evaluation description.

--- src/main/scala/com/signalcollect/triplerush/TripleVertex.scala
+++ src/main/scala/com/signalcollect/triplerush/TripleVertex.scala
@@ -7,12 +7,19 @@ class TripleVertex(id: TriplePattern) extends PatternVertex(id) {
    * Binds the queries to the pattern of this vertex and routes them to their next destinations.
    */
   def process(query: PatternQuery, graphEditor: GraphEditor[Any, Any]) {
-    for (boundQuery <- query.bind(id)) {
+    val boundQueryOption = query.bind(id)
+    if (boundQueryOption.isDefined) {
+      val boundQuery = boundQueryOption.get
       if (boundQuery.unmatched.isEmpty) {
+        // Query successful, send to query vertex.
         graphEditor.sendSignal(boundQuery, boundQuery.queryId, None)
       } else {
+        // Query not complete yet, route onwards.
         graphEditor.sendSignal(boundQuery, boundQuery.unmatched.head.routingAddress, None)
       }
+    } else {
+      // Failed to bind, send to query vertex.
+      graphEditor.sendSignal(query, query.queryId, None)
     }
   }
 }
