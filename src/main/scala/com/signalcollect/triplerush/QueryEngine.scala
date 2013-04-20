27587f56419cb95190cb58113a333ff08bf964e1
Philip Stutz <philip.stutz@gmail.com>
2013-04-20 16:11:27 +0200

    Good performance again.

--- src/main/scala/com/signalcollect/triplerush/QueryEngine.scala
+++ src/main/scala/com/signalcollect/triplerush/QueryEngine.scala
@@ -32,8 +32,8 @@ import com.signalcollect.factory.messagebus.BulkAkkaMessageBusFactory
 
 case class QueryEngine() {
 //  private val g = GraphBuilder.withMessageBusFactory(new ParallelBulkAkkaMessageBusFactory(1024)).build
-//  private val g = GraphBuilder.withMessageBusFactory(new BulkAkkaMessageBusFactory(1024, false)).build
-  private val g = GraphBuilder.build
+  private val g = GraphBuilder.withMessageBusFactory(new BulkAkkaMessageBusFactory(1024, false)).build
+//  private val g = GraphBuilder.build
   g.setUndeliverableSignalHandler { (signal, id, sourceId, graphEditor) =>
     signal match {
       case query: PatternQuery =>
