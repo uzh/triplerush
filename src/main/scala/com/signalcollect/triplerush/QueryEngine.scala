288e5c7aba9ab0a174fe1e5105c636bd95f2c830
Philip Stutz <philip.stutz@gmail.com>
2013-07-07 22:16:33 +0200

    Slower heartbeat and larger bulk messages.

--- src/main/scala/com/signalcollect/triplerush/QueryEngine.scala
+++ src/main/scala/com/signalcollect/triplerush/QueryEngine.scala
@@ -167,7 +167,8 @@ case class QueryEngine(
 
   println("Graph engine is initializing ...")
   private val g = graphBuilder.withConsole(console).
-    withMessageBusFactory(new CombiningMessageBusFactory(1024, false)).
+    withMessageBusFactory(new CombiningMessageBusFactory(4096, false)).
+    withHeartbeatInterval(500).
     withKryoRegistrations(List(
       "com.signalcollect.triplerush.TriplePattern",
       "com.signalcollect.triplerush.BindingIndexVertex",
