a0db3ff52402230330246565b422cf80dfe70624
Philip Stutz <philip.stutz@gmail.com>
2014-01-09 18:27:28 +0100

    Removed unnecessary imports.

--- src/main/scala/com/signalcollect/triplerush/TripleRush.scala
+++ src/main/scala/com/signalcollect/triplerush/TripleRush.scala
@@ -23,31 +23,28 @@ package com.signalcollect.triplerush
 import java.io.DataInputStream
 import java.io.EOFException
 import java.io.FileInputStream
-import java.util.concurrent.TimeUnit
-import scala.annotation.elidable
-import scala.annotation.elidable.ASSERTION
-import scala.collection.mutable.UnrolledBuffer
+
+import scala.concurrent.Await
 import scala.concurrent.Future
 import scala.concurrent.Promise
-import scala.concurrent.duration._
+import scala.concurrent.duration.DurationInt
+
 import org.semanticweb.yars.nx.parser.NxParser
+
 import com.signalcollect.ExecutionConfiguration
 import com.signalcollect.GraphBuilder
 import com.signalcollect.GraphEditor
+import com.signalcollect.Vertex
 import com.signalcollect.configuration.ActorSystemRegistry
 import com.signalcollect.configuration.ExecutionMode
+import com.signalcollect.interfaces.AggregationOperation
 import com.signalcollect.triplerush.QueryParticle.arrayToParticle
 import com.signalcollect.triplerush.vertices.POIndex
 import com.signalcollect.triplerush.vertices.QueryOptimizer
 import com.signalcollect.triplerush.vertices.QueryVertex
+import com.signalcollect.triplerush.vertices.RootIndex
 import com.signalcollect.triplerush.vertices.SOIndex
 import com.signalcollect.triplerush.vertices.SPIndex
-import akka.util.Timeout
-import scala.concurrent.Await
-import com.signalcollect.interfaces.AggregationOperation
-import com.signalcollect.Vertex
-import com.signalcollect.interfaces.ModularAggregationOperation
-import com.signalcollect.triplerush.vertices.RootIndex
 
 case object RegisterQueryResultRecipient
 
