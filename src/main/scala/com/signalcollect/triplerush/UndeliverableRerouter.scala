80077cdb5c12b081af8ca0fb2781e2251870b883
Philip Stutz <philip.stutz@gmail.com>
2014-01-14 17:53:27 +0100

    Simplified TripleRush.scala by factoring other classes into separate files.

---
+++ src/main/scala/com/signalcollect/triplerush/UndeliverableRerouter.scala
@@ -0,0 +1,40 @@
+/*
+ *  @author Philip Stutz
+ *  @author Mihaela Verman
+ *  
+ *  Copyright 2013 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+package com.signalcollect.triplerush
+
+import com.signalcollect.GraphEditor
+
+import QueryParticle.arrayToParticle
+
+case object UndeliverableRerouter {
+  def handle(signal: Any, targetId: Any, sourceId: Option[Any], graphEditor: GraphEditor[Any, Any]) {
+    signal match {
+      case queryParticle: Array[Int] =>
+        graphEditor.sendSignal(queryParticle.tickets, queryParticle.queryId, None)
+      case CardinalityRequest(forPattern: TriplePattern, requestor: AnyRef) =>
+        graphEditor.sendSignal(CardinalityReply(forPattern, 0), requestor, None)
+      case CardinalityReply(forPattern, cardinality) =>
+      // Do nothing, query vertex has removed itself already because of a 0 cardinality pattern.
+      case other =>
+        println(s"Failed signal delivery of $other of type ${other.getClass} to the vertex with id $targetId and sender id $sourceId.")
+    }
+  }
+}
---
+++ src/main/scala/com/signalcollect/triplerush/loading/VertexCounting.scala
@@ -0,0 +1,52 @@
+/*
+ *  @author Philip Stutz
+ *  @author Mihaela Verman
+ *  
+ *  Copyright 2013 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+package com.signalcollect.triplerush.loading
+
+import com.signalcollect.interfaces.AggregationOperation
+import com.signalcollect.Vertex
+
+case class EdgesPerIndexType() extends AggregationOperation[Map[String, Int]] {
+  def extract(v: Vertex[_, _]): Map[String, Int] = {
+    Map(v.getClass.toString -> v.edgeCount).withDefaultValue(0)
+  }
+  def reduce(elements: Stream[Map[String, Int]]): Map[String, Int] = {
+    val result: Map[String, Int] = elements.reduce { (m1: Map[String, Int], m2: Map[String, Int]) =>
+      val keys = m1.keys ++ m2.keys
+      val merged = keys.map(k => (k, m1(k) + m2(k)))
+      merged.toMap.withDefaultValue(0)
+    }
+    result
+  }
+}
+
+case class CountVerticesByType() extends AggregationOperation[Map[String, Int]] {
+  def extract(v: Vertex[_, _]): Map[String, Int] = {
+    Map(v.getClass.toString -> 1).withDefaultValue(0)
+  }
+  def reduce(elements: Stream[Map[String, Int]]): Map[String, Int] = {
+    val result: Map[String, Int] = elements.reduce { (m1: Map[String, Int], m2: Map[String, Int]) =>
+      val keys = m1.keys ++ m2.keys
+      val merged = keys.map(k => (k, m1(k) + m2(k)))
+      merged.toMap.withDefaultValue(0)
+    }
+    result
+  }
+}
