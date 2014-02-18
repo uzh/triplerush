f99c1bf23b354d09732b1a8d768748ff58fa9956
Philip Stutz <philip.stutz@gmail.com>
2014-02-18 14:51:22 +0100

    Changed dependency to signal-collect-torque, because TR uses Torque deployment.

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object GraphsBuild extends Build {
-  lazy val scCore = ProjectRef(file("../signal-collect"), id = "signal-collect")
+  lazy val scCore = ProjectRef(file("../signal-collect-torque"), id = "signal-collect-torque")
   val scTriplerush = Project(id = "triplerush",
     base = file(".")) dependsOn (scCore)
 }
