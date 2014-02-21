3d71bdfa118251db7694d86c5919205b80804ba1
Philip Stutz <philip.stutz@gmail.com>
2014-02-21 17:33:07 +0100

    Made the optimiser creators separate objects, such that they can be configured and instantiated with a path.

--- src/main/scala/com/signalcollect/triplerush/optimizers/Optimizer.scala
+++ src/main/scala/com/signalcollect/triplerush/optimizers/Optimizer.scala
@@ -9,27 +9,14 @@ trait Optimizer {
   override def toString = this.getClass.toString
 }
 
-object Optimizer {
-  val none: TripleRush => Option[Optimizer] = {
-    tr: TripleRush =>
-      None
-  }
-  val greedy: TripleRush => Option[Optimizer] = {
-    tr: TripleRush =>
-      Some(GreedyCardinalityOptimizer)
-  }
-  val clever: TripleRush => Option[Optimizer] = {
-    tr: TripleRush =>
-      Some(CleverCardinalityOptimizer)
-  }
-  val predicateSelectivity: TripleRush => Option[Optimizer] = {
-    tr: TripleRush =>
-      val stats = new PredicateSelectivity(tr)
-      Some(new CleverPredicateSelectivityOptimizer(stats))
-  }
-  val bibekPredicateSelectivity: TripleRush => Option[Optimizer] = {
-    tr: TripleRush =>
-      val stats = new PredicateSelectivity(tr)
-      Some(new PredicateSelectivityOptimizer(stats))
-  }
+object NoOptimizerCreator extends Function1[TripleRush, Option[Optimizer]] {
+  def apply(tr: TripleRush) = None
+}
+
+object GreedyOptimizerCreator extends Function1[TripleRush, Option[Optimizer]] {
+  def apply(tr: TripleRush) = Some(GreedyCardinalityOptimizer)
+}
+
+object CleverOptimizerCreator extends Function1[TripleRush, Option[Optimizer]] {
+  def apply(tr: TripleRush) = Some(CleverCardinalityOptimizer)
 }
