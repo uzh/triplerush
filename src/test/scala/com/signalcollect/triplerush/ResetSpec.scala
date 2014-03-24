79635d5c06577557509f75b4c62c3322956be45d
Philip Stutz <philip.stutz@gmail.com>
2014-03-24 18:30:30 +0100

    Testing the TR reset feature.

---
+++ src/test/scala/com/signalcollect/triplerush/ResetSpec.scala
@@ -0,0 +1,29 @@
+package com.signalcollect.triplerush
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.scalacheck.Arbitrary
+import com.signalcollect.triplerush.util.ArrayOfArraysTraversable
+import org.scalatest.prop.Checkers
+import org.scalacheck.Gen._
+import com.signalcollect.triplerush.optimizers.CleverCardinalityOptimizer
+
+class ResetSpec extends FlatSpec with Matchers with TestAnnouncements {
+
+  "TripleRush" should "correctly function even after a reset" in {
+    val tr = new TripleRush
+    try {
+      tr.addTriplePattern(TriplePattern(1, 2, 3))
+      tr.prepareExecution
+      val query = List(TriplePattern(-1, 2, 3))
+      val result1 = tr.executeQuery(QuerySpecification(query), Some(CleverCardinalityOptimizer))
+      tr.clear
+      tr.addTriplePattern(TriplePattern(3, 2, 3))
+      tr.prepareExecution
+      val result2 = tr.executeQuery(QuerySpecification(query), Some(CleverCardinalityOptimizer))
+    } finally {
+      tr.shutdown
+    }
+  }
+
+}
