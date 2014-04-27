60072a4815a2a7d8d79a53c9c346fc298819508b
Bibek <paudel.bibek@gmail.com>
2014-04-27 13:08:21 +0200

    Added dependency on signal-collect-slurm.

--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,7 @@ import Keys._
 
 object GraphsBuild extends Build {
   lazy val scCore = ProjectRef(file("../signal-collect-torque"), id = "signal-collect-torque")
+  lazy val scSlurm = ProjectRef(file("../signal-collect-slurm"), id = "signal-collect-slurm")
   val scTriplerush = Project(id = "triplerush",
-    base = file(".")) dependsOn (scCore)
+    base = file(".")) dependsOn (scCore, scSlurm)
 }
