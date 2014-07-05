adae36fcc9145b7fa8ea78cc886870beb56f03d2
Philip Stutz <philip.stutz@gmail.com>
2014-07-05 18:27:41 +0200

    Removed torque dependency again.
    Less clutter for the single-machine version.

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object GraphsBuild extends Build {
-  lazy val scCore = ProjectRef(file("../signal-collect-torque"), id = "signal-collect-torque")
+  lazy val scCore = ProjectRef(file("../signal-collect"), id = "signal-collect")
   val scTriplerush = Project(id = "triplerush",
     base = file(".")) dependsOn (scCore)
 }
