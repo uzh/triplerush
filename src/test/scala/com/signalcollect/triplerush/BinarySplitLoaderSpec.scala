2e28f4092ad3ee1fc39ef81fbcc0417d6e0ba2d6
Philip Stutz <philip.stutz@gmail.com>
2014-08-14 15:38:09 +0200

    Additional tests for the binary split loader.

--- src/test/scala/com/signalcollect/triplerush/BinarySplitLoaderSpec.scala
+++ src/test/scala/com/signalcollect/triplerush/BinarySplitLoaderSpec.scala
@@ -4,8 +4,51 @@ import org.scalatest.FlatSpec
 import java.io.File
 
 class BinarySplitLoaderSpec extends FlatSpec with TestAnnouncements {
+
   "BinarySplitLoader" should "load a file" in {
     val tr = new TripleRush
+    try {
+      val sep = File.separator
+      for (splitId <- 0 to 3) {
+        val filename = s".${sep}lubm${sep}lubm1-filtered-splits${sep}$splitId.filtered-split"
+        tr.loadBinary(filename, Some(splitId))
+      }
+      tr.prepareExecution
+      val iter = tr.resultIteratorForQuery(Array(TriplePattern(-1, -2, -3)))
+      var resultCount = 0
+      while (iter.hasNext) {
+        val n = iter.next
+        resultCount += 1
+      }
+      assert(resultCount == 1711)
+    } finally {
+      tr.shutdown
+    }
+  }
+
+  it should "work with the alternative triple mapper" in {
+    val tr = new TripleRush(tripleMapperFactory = Some(new AlternativeTripleMapperFactory(false)))
+    try {
+      val sep = File.separator
+      for (splitId <- 0 to 3) {
+        val filename = s".${sep}lubm${sep}lubm1-filtered-splits${sep}$splitId.filtered-split"
+        tr.loadBinary(filename, Some(splitId))
+      }
+      tr.prepareExecution
+      val iter = tr.resultIteratorForQuery(Array(TriplePattern(-1, -2, -3)))
+      var resultCount = 0
+      while (iter.hasNext) {
+        val n = iter.next
+        resultCount += 1
+      }
+      assert(resultCount == 1711)
+    } finally {
+      tr.shutdown
+    }
+  }
+
+  it should "work with the load balancing triple mapper" in {
+    val tr = new TripleRush(tripleMapperFactory = Some(LoadBalancingTripleMapperFactory))
     try {
       val sep = File.separator
       for (splitId <- 0 to 3) {
