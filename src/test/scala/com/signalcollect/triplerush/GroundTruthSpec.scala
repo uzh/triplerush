584552d36bec20865bdde4f2edcf157f98a06c96
Philip Stutz <philip.stutz@gmail.com>
2015-07-15 13:43:13 +0200

    Fixed query.

--- src/test/scala/com/signalcollect/triplerush/GroundTruthSpec.scala
+++ src/test/scala/com/signalcollect/triplerush/GroundTruthSpec.scala
@@ -55,10 +55,10 @@ object Lubm {
     """
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX ub: <http://swat.cse.lehigh.edu/onto/univ-bench.owl#>
-SELECT ?X	
+SELECT ?X
 WHERE {
   ?X rdf:type ub:GraduateStudent .
-  ?X ub:takesCourse "http://www.Department0.University0.edu/GraduateCourse0"
+  ?X ub:takesCourse <http://www.Department0.University0.edu/GraduateCourse0>
 }
 """,
     """
