823331a5f971dce8d1d043779ff1000c6c46eca6
Jahangir Mohammed <md.jahangir27@gmail.com>
2015-07-22 00:52:45 +0530

    Run tests in syntax-fed against triple rush.

--- src/main/scala/com/signalcollect/triplerush/sparql/Sparql11SyntaxTest.scala
+++ src/main/scala/com/signalcollect/triplerush/sparql/Sparql11SyntaxTest.scala
@@ -1,12 +1,13 @@
 package com.signalcollect.triplerush.sparql
 
+import com.hp.hpl.jena.query.QueryParseException
 import com.signalcollect.triplerush.TripleRush
 import org.scalatest.{Matchers, FlatSpec}
 import scala.collection.JavaConversions.asScalaIterator
 
 class Sparql11SyntaxTest extends FlatSpec with Matchers{
   
-  it should "read manifest.ttl of syntax-fed and write a query against to get tests and their type in them" in {
+  "TripleRush" should "pass tests in /syntax-fed" in {
     val tr = new TripleRush
     val manifestFile = "src/test/resources/sparql-1.1-w3c/syntax-fed/manifest.ttl"
     val graph = new TripleRushGraph(tr)
@@ -20,16 +21,38 @@ class Sparql11SyntaxTest extends FlatSpec with Matchers{
         |   PREFIX dawgt: <http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#>
         |   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         |				SELECT ?TestURI ?Name ?Action ?Type
-        |		WHERE { [] rdf:first ?TestURI.
-        |		        ?TestURI a ?Type ;
+        |	     	WHERE { [] rdf:first ?TestURI.
+        |		           ?TestURI a ?Type ;
         |		                 mf:name ?Name ;
         |		                 mf:action ?Action ;
         |		                 dawgt:approval dawgt:Approved .
-        |		        FILTER(?Type IN (mf:PositiveSyntaxTest11, mf:NegativeSyntaxTest11, mf:PositiveUpdateSyntaxTest11, mf:NegativeUpdateSyntaxTest11))
-        |		 }
+        |		           }
       """.stripMargin
     val results = Sparql(query)
-    results.size should be(3)
+
+    case class TestDetails(uri: String, name: String, action: String, positive: Boolean)
+    
+    val testsToRun = results.map(test => {
+        val uriOfTest = test.get("TestURI").toString
+        val nameOfTest = test.get("Name").toString
+        val action = test.get("Action").toString
+        val typeOfTest = test.get("Type").toString
+        val positiveTest = typeOfTest.equals("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#PositiveSyntaxTest11")
+        TestDetails(uriOfTest, nameOfTest, action, positiveTest)
+      }
+    ).toList
+
+    //TODO: Need to figure why java.lang.IllegalStateException is thrown for positive syntax test.
+    testsToRun.map(test => {
+      val query = scala.io.Source.fromFile(test.action.replace("file://","")).mkString
+      if(test.positive) {
+          Sparql(query)
+      }
+      else {
+        intercept[QueryParseException] {
+          Sparql(query)
+        }
+      }
+    })
   }
-  
 }
