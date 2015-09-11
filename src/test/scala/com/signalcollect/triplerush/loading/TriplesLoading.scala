f69332f2e291579310dc3e2c53960a827fe0d3ed
Philip Stutz <philip.stutz@gmail.com>
2015-09-12 01:44:25 +0200

    Added gzip triples loading example

---
+++ src/test/scala/com/signalcollect/triplerush/loading/TriplesLoading.scala
@@ -0,0 +1,18 @@
+package com.signalcollect.triplerush.loading
+
+import java.io.{ FileInputStream, InputStream }
+import java.util.zip.GZIPInputStream
+
+import org.apache.jena.riot.Lang
+
+import com.signalcollect.triplerush.TripleRush
+
+object TriplesLoading extends App {
+
+  val tr = new TripleRush(fastStart = true)
+  val inputStream = new FileInputStream(args(0))
+  val gzipInputStream: InputStream = new GZIPInputStream(inputStream)
+  tr.loadStream(gzipInputStream, lang = Lang.NT)
+  tr.awaitIdle()
+
+}
