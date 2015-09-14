52e799cf6666701e4da673fd47e043c8b75629f4
Philip Stutz <philip.stutz@gmail.com>
2015-09-14 22:17:27 +0200

    Loaded 33 million triples into 1GB of off-heap dictionary memory

--- src/test/scala/com/signalcollect/triplerush/dictionary/DictionaryBenchmark.scala
+++ src/test/scala/com/signalcollect/triplerush/dictionary/DictionaryBenchmark.scala
@@ -1,8 +1,16 @@
 package com.signalcollect.triplerush.dictionary
 
-import org.mapdb.DBMaker
+import java.io.{ FileInputStream, InputStream }
+import java.util.concurrent.Executors
+import java.util.zip.GZIPInputStream
+
+import scala.collection.JavaConversions.asScalaIterator
 import scala.util.Random
-import java.util.Arrays
+
+import org.apache.jena.graph.{ Triple => JenaTriple }
+import org.apache.jena.riot.{ Lang, RDFDataMgr }
+import org.apache.jena.riot.lang.{ PipedRDFIterator, PipedTriplesStream }
+import org.mapdb.DBMaker
 
 object DictionaryBenchmark extends App {
 
@@ -29,20 +37,65 @@ object DictionaryBenchmark extends App {
     .asyncWriteQueueSize(asyncQueueSize)
     .compressionEnable
 
-  val warmupStrings = 100000
-  val timedStrings = 1000000
+  val warmupStrings = 10
+  val timedStrings = 100000000
   val maxId = warmupStrings + timedStrings
 
   val dictionary = new HashDictionary(id2StringNodeSize, string2IdNodeSize, dbMaker)
-  addStrings(warmupStrings)
-  addStrings(timedStrings, Some(s"PUTS: id2StringNodeSize=$id2StringNodeSize string2IdNodeSize=$string2IdNodeSize asyncQueueSize=$asyncQueueSize"))
+
+  val startTime = System.currentTimeMillis
+  val inputStream = new FileInputStream(args(0))
+  val gzipInputStream: InputStream = new GZIPInputStream(inputStream)
+  val tripleIterator = new PipedRDFIterator[JenaTriple]
+  val sink = new PipedTriplesStream(tripleIterator)
+  val executor = Executors.newSingleThreadExecutor
+  val parser = new Runnable {
+    def run: Unit = RDFDataMgr.parse(sink, gzipInputStream, Lang.NTRIPLES)
+  }
+  executor.submit(parser)
+  var triplesAdded = 0
+  val stringIterator = tripleIterator.flatMap { triple =>
+    triplesAdded += 1
+    if (triplesAdded % 10000 == 0) {
+      val seconds = ((System.currentTimeMillis - startTime) / 100.0).round / 10.0
+      println(s"$triplesAdded triples loaded after $seconds seconds: $dictionary")
+    }
+    List(triple.getSubject.toString, triple.getPredicate.toString, triple.getObject.toString(true))
+  }
+
+  addStrings(warmupStrings, generatingIterator())
+  addStrings(timedStrings, stringIterator,
+    Some(s"PUTS: id2StringNodeSize=$id2StringNodeSize string2IdNodeSize=$string2IdNodeSize asyncQueueSize=$asyncQueueSize"))
+
   println(dictionary)
-  
-  def addStrings(howMany: Int, timed: Option[String] = None): Unit = {
-    def run(): Unit = {
+  tripleIterator.close
+  gzipInputStream.close
+  inputStream.close
+  executor.shutdownNow
+
+  def generatingIterator(size: Int = Int.MaxValue): Iterator[String] = {
+    val startTime = System.currentTimeMillis
+    new Iterator[String] {
+      var count = 0
+      def next = {
+        count += 1
+        if (count % 10000 == 0) {
+          val seconds = ((System.currentTimeMillis - startTime) / 100.0).round / 10.0
+          println(s"Generating iterator: $count strings loaded after $seconds seconds")
+        }
+        generateString
+      }
+      def hasNext: Boolean = {
+        count < size
+      }
+    }
+  }
+
+  def addStrings(howMany: Int, iter: Iterator[String], timed: Option[String] = None): Unit = {
+    def run(s: Iterator[String]): Unit = {
       var i = 0
       while (i < howMany) {
-        dictionary(generateString)
+        dictionary(s.next)
         i += 1
       }
     }
@@ -50,10 +103,10 @@ object DictionaryBenchmark extends App {
       case Some(name) =>
         println(s"Adding $howMany entries ...")
         time {
-          run //(strings)
+          run(iter)
         }(name, Some(howMany))
       case None =>
-        run //(strings)
+        run(iter)
     }
   }
 
