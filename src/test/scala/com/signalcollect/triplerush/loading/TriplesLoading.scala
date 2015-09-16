4e1eba8bf5a0f43ba44649a72a9c056d4369741e
Philip Stutz <philip.stutz@gmail.com>
2015-09-16 14:46:18 +0200

    Adapted TriplesLoading to new loading API

--- src/test/scala/com/signalcollect/triplerush/loading/TriplesLoading.scala
+++ src/test/scala/com/signalcollect/triplerush/loading/TriplesLoading.scala
@@ -27,13 +27,10 @@ import org.apache.jena.riot.Lang
 
 import com.signalcollect.triplerush.TripleRush
 
-// TODO: Rework with new `TripleIterator`
 object TriplesLoading extends App {
 
   val tr = TripleRush(fastStart = true)
-  val inputStream = new FileInputStream(args(0))
-  val gzipInputStream: InputStream = new GZIPInputStream(inputStream)
-  tr.loadStream(gzipInputStream, lang = Lang.NT)
+  tr.loadFromFile(args(0))
   tr.awaitIdle()
 
 }
