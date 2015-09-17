191e8e3b6fc274d4f0b1e24cf436af66ce458160
Philip Stutz <philip.stutz@gmail.com>
2015-09-17 14:04:08 +0200

    Fixed bug in HashDictionary, improved hashing

--- src/main/scala/com/signalcollect/triplerush/dictionary/HashDictionary.scala
+++ src/main/scala/com/signalcollect/triplerush/dictionary/HashDictionary.scala
@@ -24,13 +24,42 @@ import java.nio.charset.Charset
 import java.util.Arrays
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
-
 import scala.annotation.tailrec
 import scala.util.hashing.MurmurHash3
-
 import org.mapdb.{ BTreeKeySerializer, DBMaker }
 import org.mapdb.DBMaker.Maker
 import org.mapdb.Serializer
+import org.mapdb.DataIO
+
+final object HashDictionary {
+
+  private[this] val utf8 = Charset.forName("UTF-8")
+
+  val seed = 2147483647
+  val prefixBytes = 20
+  val suffixBytes = 20
+  val httpBytes = 7
+  val fullHashBelow = math.max(40, prefixBytes + suffixBytes + httpBytes)
+  val asciiH = 'h'.toByte
+
+  @inline def hash(bytes: Array[Byte]): Int = {
+    val l = bytes.length
+    val hash: Long = if (l < fullHashBelow) {
+      DataIO.hash(bytes, 0, l, seed)
+    } else {
+      val isProbablyUri = bytes(0) == asciiH
+      val prefixHash = if (isProbablyUri) {
+        DataIO.hash(bytes, httpBytes, prefixBytes, seed)
+      } else {
+        DataIO.hash(bytes, 0, prefixBytes, seed)
+      }
+      val suffixHash = DataIO.hash(bytes, l - suffixBytes, suffixBytes, seed)
+      prefixHash ^ suffixHash
+    }
+    DataIO.longHash(hash) & Int.MaxValue
+  }
+
+}
 
 final class HashDictionary(
     val id2StringNodeSize: Int = 32,
@@ -75,17 +104,12 @@ final class HashDictionary(
   private[this] def addEntry(s: Array[Byte], idCandidate: Int): Int = {
     val existing = id2String.putIfAbsent(idCandidate, s)
     if (existing == null) {
-      //      println(s"Added string ${new String(s, utf8)} with hash-based ID $idCandidate")
-      //      assert(id2String.containsKey(idCandidate)) // TODO: Verify MapDB beta 7 fixes this.
       idCandidate
     } else {
       if (Arrays.equals(s, existing)) {
-        //        println(s"Did not add string ${new String(s, utf8)}, because it already exists in the dictionary with ID $idCandidate")
         idCandidate // existing
       } else {
-        val id = addEntryToExceptions(s) // collision
-        //        println(s"Collision of new string ${new String(s, utf8)} with existing string ${new String(existing, utf8)}, added it with exception ID $id")
-        id
+        addEntryToExceptions(s) // collision
       }
     }
   }
@@ -113,7 +137,7 @@ final class HashDictionary(
 
   def contains(s: String): Boolean = {
     val stringBytes = s.getBytes(utf8)
-    val idCandidate = fastHash(stringBytes)
+    val idCandidate = HashDictionary.hash(stringBytes)
     val existing = id2String.get(idCandidate)
     if (existing == null) {
       false
@@ -126,30 +150,37 @@ final class HashDictionary(
 
   def apply(s: String): Int = {
     val stringBytes = s.getBytes(utf8)
-    val idCandidate = fastHash(stringBytes)
+    val idCandidate = HashDictionary.hash(stringBytes)
     val existing = id2String.get(idCandidate)
-    if (existing != null && Arrays.equals(stringBytes, existing)) {
-      idCandidate
-    } else {
+    if (existing == null) {
+      //      println(s"added $s with hash-id $idCandidate")
       addEntry(stringBytes, idCandidate)
+    } else {
+      if (Arrays.equals(stringBytes, existing)) {
+        //        println(s"$s existed already with hash-id $idCandidate")
+        idCandidate
+      } else {
+        val exceptionId = string2Id.get(stringBytes)
+        if (exceptionId != 0) {
+          //          println(s"$s existed already with exception-id $exceptionId")
+          exceptionId
+        } else {
+          val id = addEntryToExceptions(stringBytes)
+          //          println(s"$s added with exception-id $id")
+          id
+        }
+      }
     }
   }
 
   def apply(id: Int): String = {
-    if (id == 0) {
-      "*"
-    } else {
-      val bytes = id2String.get(id)
-      new String(bytes, utf8)
-    }
+    val bytes = id2String.get(id)
+    new String(bytes, utf8)
   }
 
   def get(id: Int): Option[String] = {
-    if (id == 0) {
-      Some("*")
-    } else {
-      Option(id2String.get(id)).map(new String(_, utf8))
-    }
+    val bytes = id2String.get(id)
+    Option(bytes).map(new String(_, utf8))
   }
 
   def contains(i: Int): Boolean = {
@@ -162,66 +193,27 @@ final class HashDictionary(
 
   def get(s: String): Option[Int] = {
     val stringBytes = s.getBytes(utf8)
-    val idCandidate = fastHash(stringBytes)
+    val idCandidate = HashDictionary.hash(stringBytes)
     val existing = id2String.get(idCandidate)
-    if (existing != null) {
+    if (existing == null) {
+      None
+    } else {
       if (Arrays.equals(stringBytes, existing)) {
         Some(idCandidate)
       } else {
-        val exception = string2Id.get(stringBytes)
-        if (exception != 0) {
-          Some(exception)
+        val exceptionId = string2Id.get(stringBytes)
+        if (exceptionId != 0) {
+          Some(exceptionId)
         } else {
           None
         }
       }
-    } else {
-      None
     }
   }
 
   def close(): Unit = {
     string2Id.close()
     id2String.close()
-  }
-
-  private[this] val suffixChars = 45
-  private[this] val prefixChars = 5
-  private[this] val fullStringUptTo = 50
-
-  @inline private[this] def fastHash(b: Array[Byte]): Int = {
-    if (b.length == 0) {
-      return 0
-    }
-    var hash = 2147483647 // large prime.
-    var i = 0
-    val l = b.length - 1
-    if (b.length <= fullStringUptTo) {
-      while (i < l) {
-        hash = MurmurHash3.mix(hash, b(i))
-        i += 1
-      }
-    } else {
-      // Skip `http://www.`
-      i = if (b(0) == 'h') {
-        11
-      } else {
-        0
-      }
-      val prefixEndIndex = math.min(l, i + prefixChars)
-      while (i < prefixEndIndex) {
-        hash = MurmurHash3.mix(hash, b(i))
-        i += 1
-      }
-      i = math.max(i, b.length - suffixChars)
-      while (i < l) {
-        hash = MurmurHash3.mix(hash, b(i))
-        i += 1
-      }
-    }
-    hash = MurmurHash3.mixLast(hash, b(i))
-    hash = MurmurHash3.finalizeHash(hash, 3)
-    hash & Int.MaxValue
   }
 
   override def toString = s"HashDictionary(id2String=${id2String.size}, string2Id=${string2Id.size})"
