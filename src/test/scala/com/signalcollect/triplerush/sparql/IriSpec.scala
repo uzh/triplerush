091dfd7f3e7aed892369430302a20ea4820e32a7
Philip Stutz <philip.stutz@gmail.com>
2015-11-24 12:22:56 +0100

    Added test for uncommon IRI, fixes #49

---
+++ src/test/scala/com/signalcollect/triplerush/sparql/IriSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015 Cotiviti Labs (nexgen.admin@cotiviti.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.signalcollect.triplerush.sparql
+
+import scala.collection.JavaConversions.asScalaIterator
+
+import org.scalatest.fixture.{ FlatSpec, UnitFixture }
+
+import com.signalcollect.triplerush.TestStore
+
+class IriSpec extends FlatSpec with UnitFixture {
+
+  "TripleRush" should "support uncommon IRIs" in new TestStore {
+    val urn = "urn:uuid:123"
+    tr.addStringTriple(urn, urn, "\"test\"")
+    val sparql = """
+SELECT ?p WHERE {
+  {
+      <urn:uuid:123> ?p ?o .
+  }
+}"""
+    val results = Sparql(sparql)
+    val resultBindings = results.map(_.get("p").toString).toList
+    assert(resultBindings === List(urn))
+  }
+
+}
