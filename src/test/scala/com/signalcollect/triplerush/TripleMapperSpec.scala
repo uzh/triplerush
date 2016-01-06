62b0b4527b078c5918672b0e9fdab38e08d1ba89
Philip Stutz <philip.stutz@gmail.com>
2016-01-06 21:54:56 +0100

    Improved triple mapper tests, fixes issue #63

--- src/test/scala/com/signalcollect/triplerush/TripleMapperSpec.scala
+++ src/test/scala/com/signalcollect/triplerush/TripleMapperSpec.scala
@@ -18,69 +18,94 @@
 
 package com.signalcollect.triplerush
 
+import org.scalacheck.{ Arbitrary, Gen, Prop }
+import org.scalacheck.Prop.propBoolean
+import org.scalatest.{ Finders, FlatSpec, Matchers }
 import org.scalatest.prop.Checkers
-import org.scalacheck.Prop._
-import org.scalatest.FlatSpec
-import org.scalatest.Matchers
-import com.signalcollect.triplerush.mapper.DistributedTripleMapper
-import com.signalcollect.triplerush.mapper.RelievedNodeZeroTripleMapper
-import org.scalacheck.Gen
-import org.scalacheck.Prop
+
+import com.signalcollect.triplerush.mapper.{ DistributedTripleMapper, RelievedNodeZeroTripleMapper }
 
 class TripleMapperSpec extends FlatSpec with Matchers with Checkers {
 
-  val numberOfNodes = 8
-  val workersPerNode = 24
-  val step = numberOfNodes * workersPerNode
+  case class NodeSetup(numberOfNodes: Int, workersPerNode: Int) {
+    private[this] val step = numberOfNodes * workersPerNode
+    assert(numberOfNodes > 0)
+    assert(workersPerNode > 0)
+    def nodeId(workerId: Int) = (((workerId & Int.MaxValue) % step) / workersPerNode).floor.toInt
+    def nodeRelativeWorkerId(workerId: Int) = (workerId & Int.MaxValue) % workersPerNode
+  }
 
-  val distributedMapper = new DistributedTripleMapper(numberOfNodes = numberOfNodes, workersPerNode = workersPerNode)
-  val relievedNodeZeroMapper = new RelievedNodeZeroTripleMapper(numberOfNodes = numberOfNodes, workersPerNode = workersPerNode)
+  val genPositiveInt = Gen.choose(1, 100)
 
-  def nodeId(workerId: Int) = (((workerId & Int.MaxValue) % step) / workersPerNode).floor.toInt
+  val genZeroOrPositive = Gen.oneOf(Gen.const(0), genPositiveInt)
 
-  def workerId(workerId: Int) = (workerId & Int.MaxValue) % workersPerNode
+  val genIndexPattern = {
+    val genPattern = for {
+      s <- genZeroOrPositive
+      p <- genZeroOrPositive
+      o <- genZeroOrPositive
+    } yield TriplePattern(s, p, o)
+    genPattern.filter(!_.isFullyBound)
+  }
 
-  assert(nodeId(0) == 0)
-  assert(nodeId(1) == 0)
-  assert(nodeId(3) == 0)
-  assert(nodeId(24) == 1)
-  assert(nodeId(191) == 7)
-  assert(nodeId(25) == 1)
-  assert(workerId(25) == 1)
-  assert(workerId(1) == 1)
-  assert(workerId(191) == 23)
+  implicit val arbIndexPattern = Arbitrary(genIndexPattern)
 
-  val genPositiveInt = Gen.choose(1, Int.MaxValue)
+  val genNodeSetup = {
+    for {
+      nodes <- Gen.choose(1, 13)
+      workersPerNode <- Gen.choose(1, 17)
+    } yield NodeSetup(nodes, workersPerNode)
+  }
+
+  implicit val arbNodeSetup = Arbitrary(genNodeSetup)
+
+  def distributedMapper(s: NodeSetup) = new DistributedTripleMapper(numberOfNodes = s.numberOfNodes, workersPerNode = s.workersPerNode)
+  def relievedNodeZeroMapper(s: NodeSetup) = new RelievedNodeZeroTripleMapper(numberOfNodes = s.numberOfNodes, workersPerNode = s.workersPerNode)
 
   "TripleMapper" should "assign triples with the same subject to the same node" in {
     check {
-      Prop.forAll(genPositiveInt) {
-        (subjectId: Int) =>
-          val node1 = nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(subjectId, 2, 0)))
-          val node2 = nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(subjectId, 0, 5)))
-          node1 == node2
+      Prop.forAll(genPositiveInt, genNodeSetup) {
+        (subjectId: Int, s: NodeSetup) =>
+          val m = distributedMapper(s)
+          val node1 = s.nodeId(m.getWorkerIdForVertexId(EfficientIndexPattern(subjectId, 2, 0)))
+          val node2 = s.nodeId(m.getWorkerIdForVertexId(EfficientIndexPattern(subjectId, 0, 5)))
+          assert(node1 == node2)
+          true
       }
     }
   }
 
   it should "assign triples that share subject/object to the same node, if the one with the object has no subject set" in {
     check {
-      Prop.forAll(genPositiveInt) {
-        (positiveId: Int) =>
-          val node1 = nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(positiveId, 2, 0)))
-          val node2 = nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(0, 5, positiveId)))
-          node1 == node2
+      Prop.forAll(genPositiveInt, genNodeSetup) {
+        (positiveId: Int, s: NodeSetup) =>
+          val m = distributedMapper(s)
+          val node1 = s.nodeId(m.getWorkerIdForVertexId(EfficientIndexPattern(positiveId, 2, 0)))
+          val node2 = s.nodeId(m.getWorkerIdForVertexId(EfficientIndexPattern(0, 5, positiveId)))
+          assert(node1 == node2)
+          true
       }
     }
   }
 
-  it should "usually assign triples that share subject/object to different workers on the same node, when their predicates are different" in {
+  it should "assign triples that share subject/object to different workers on the same node, when their predicates are different" in {
     check {
-      Prop.forAll(genPositiveInt) {
-        (positiveId: Int) =>
-          val w1 = workerId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(positiveId, 2, 0)))
-          val w2 = workerId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(0, 5, positiveId)))
-          w1 != w2
+      Prop.forAll(genPositiveInt, genNodeSetup) {
+        (positiveId: Int, s: NodeSetup) =>
+          val m = distributedMapper(s)
+          val p1 = 2
+          val p2 = 97
+          val pDelta = p2 - p1
+          val workerId1 = m.getWorkerIdForVertexId(EfficientIndexPattern(positiveId, p1, 0))
+          val workerId2 = m.getWorkerIdForVertexId(EfficientIndexPattern(0, p2, positiveId))
+          val relativeWorkerId1 = s.nodeRelativeWorkerId(workerId1)
+          val relativeWorkerId2 = s.nodeRelativeWorkerId(workerId2)
+          assert(s.nodeId(workerId1) == s.nodeId(workerId2))
+          // Only check if there are > 1 workers and if there delta between the IDs is not a multiple of the number of workers
+          if (s.workersPerNode > 1 && pDelta % s.workersPerNode != 0) {
+            assert(relativeWorkerId1 != relativeWorkerId2)
+          }
+          true
       }
     }
   }
@@ -90,45 +115,60 @@ class TripleMapperSpec extends FlatSpec with Matchers with Checkers {
    * on the same node.
    */
   it should "not assign all the small ids to the same node" in {
-    val node1 = nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(1, 2, 0)))
-    val node2 = nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(2, 5, 0)))
-    val node3 = nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(3, 5, 0)))
-    node1 != node2
-    node2 != node3
-    node1 != node3
+    check {
+      Prop.forAll(genPositiveInt, genNodeSetup) {
+        (positiveId: Int, s: NodeSetup) =>
+          val m = distributedMapper(s)
+          val node1 = s.nodeId(m.getWorkerIdForVertexId(EfficientIndexPattern(1, 2, 0)))
+          val node2 = s.nodeId(m.getWorkerIdForVertexId(EfficientIndexPattern(2, 5, 0)))
+          val node3 = s.nodeId(m.getWorkerIdForVertexId(EfficientIndexPattern(3, 5, 0)))
+          if (s.numberOfNodes >= 3) {
+            assert(node1 != node2)
+            assert(node2 != node3)
+            assert(node1 != node3)
+          }
+          true
+      }
+    }
   }
 
   it should "always assign queries to node 0" in {
-    check((queryVertexId: Int) => {
+    check((queryVertexId: Int, s: NodeSetup) => {
       if (queryVertexId != 0 && queryVertexId != Int.MinValue) {
-        val queryWorkerId = distributedMapper.getWorkerIdForVertexId(OperationIds.embedInLong(queryVertexId))
-        val node = nodeId(queryWorkerId)
-        node == 0
-      } else {
-        true
+        val m = distributedMapper(s)
+        val queryWorkerId = m.getWorkerIdForVertexId(OperationIds.embedInLong(queryVertexId))
+        val node = s.nodeId(queryWorkerId)
+        assert(node == 0)
       }
-    }, minSuccessful(10))
+      true
+    }, minSuccessful(1000))
   }
 
   "RelievedNodeZeroMapper" should "not assign (non-root) triple patterns to node 0" in {
-    assert(nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(1, 2, 0))) != 0)
-    assert(nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(0, 7, 5))) != 0)
-    assert(nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(9, 0, 6))) != 0)
-    assert(nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(7, 0, 0))) != 0)
-    assert(nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(0, 3, 0))) != 0)
-    assert(nodeId(distributedMapper.getWorkerIdForVertexId(EfficientIndexPattern(0, 0, 2))) != 0)
+    check((tp: TriplePattern, s: NodeSetup) => {
+      assert(!tp.isFullyBound, "Index patterns cannot be fully bound.")
+      if (s.numberOfNodes > 1) {
+        val m = relievedNodeZeroMapper(s)
+        val worker = m.getWorkerIdForVertexId(tp.toEfficientIndexPattern)
+        val node = s.nodeId(worker)
+        if (tp != TriplePattern(0, 0, 0)) {
+          assert(node != 0)
+        }
+      }
+      true
+    }, minSuccessful(1000))
   }
 
   it should "always assign queries to node 0" in {
-    check((queryVertexId: Int) => {
-      if (queryVertexId != 0 && queryVertexId != Int.MinValue) {
-        val queryWorkerId = relievedNodeZeroMapper.getWorkerIdForVertexId(OperationIds.embedInLong(queryVertexId))
-        val node = nodeId(queryWorkerId)
-        node == 0
-      } else {
-        true
+    check((queryVertexId: Int, s: NodeSetup) => {
+      if (s.numberOfNodes > 1 && queryVertexId != 0 && queryVertexId != Int.MinValue) {
+        val m = relievedNodeZeroMapper(s)
+        val queryWorkerId = m.getWorkerIdForVertexId(OperationIds.embedInLong(queryVertexId))
+        val node = s.nodeId(queryWorkerId)
+        assert(node == 0)
       }
-    }, minSuccessful(10))
+      true
+    }, minSuccessful(1000))
   }
 
 }
